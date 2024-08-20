9a47c97473b9e7aafe7c97ab0be3fbff185510a4
sthefcatjup <111788708+sthefcatjup@users.noreply.github.com>
2024-08-20 16:44:56 -0300

    Create TarefaRepository.java

---
+++ TarefaRepository.java
@@ -0,0 +1,10 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.model.Tarefa;
+
+public interface TarefaRepository extends CrudRepository<Tarefa, Long> {
+
+    
+} 
